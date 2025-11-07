b3fb0d45dea84a6d7ba9cd2d85b23ea814f91c18
Owen Hill <owenhill10@gmail.com>
2025-11-07 15:47:30 -0500

    Update axios.ts

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,6 +2,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 export const request = axios.create({
-  baseURL: `${process.env.EXPO_PUBLIC_BACKEND_ENDPOINT}/api/`,
+  baseURL: `https://54.221.157.136:8800/api/`,
   withCredentials: true,
 });
